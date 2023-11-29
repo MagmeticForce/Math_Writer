638e87cfb7eb5c8365f63e83e27aace8edbd0ab6
MagmeticForce <qaz7788.br@gmail.com>
2023-11-28 22:09:03 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+package contents;
+
+public class Main {
+    public static void main(String[] args) {
+        GUI.launch(GUI.class, args);
+    }
+}
---
+++ Program_Object.java
@@ -0,0 +1,15 @@
+package contents;
+
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Program_Object {
+    
+    boolean exists = true;
+    /*
+    //these are unneccessary because you can use the "getLayoutX()" and "getLayoutY()" functions
+    double[] x = {0};
+    double[] y = {0}; //stored in an array so that value modifications are reflected outside of functions
+    */
+
+}
